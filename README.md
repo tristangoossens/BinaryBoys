8e1b5cb3310aa223782740926a8fcc051c482225
Tristan Goossens <d267316@edu.rocwb.nl>
2021-12-14 13:41:10 +0100

    initial project folder structure setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BinaryBoys
-# Sybrand schreef dit
+# Binary boys
+Project gemaakt door Sybrand Bos, Florian Burg, Sem van Elteren en Tristan Goossens
---
+++ src/GUI/App.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Binary Boys!!!!! 01101001100111011");
+    }
+}
