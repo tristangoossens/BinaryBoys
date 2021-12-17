8d5b650a472af666294b37d34d86eafbf3b00e3b
Sybrand Bos <snjw.bos@student.avans.nl>
2021-12-17 16:13:05 +0100

    student button

--- src/Database/Conn.java
+++ src/Database/Conn.java
@@ -3,17 +3,17 @@ package Database;
 import java.sql.*;
 
 public class Conn {
-    // Protected attribute so it can be accessed within the package 
+    // Protected attribute so it can be accessed within the package
     protected Connection conn;
 
-    public Conn(){
+    public Conn() {
         String connURL = "jdbc:sqlserver://localhost;databaseName=CodeCademy;integratedSecurity=true;";
 
         try {
             // Import db driver and connect to database with connection string
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             this.conn = DriverManager.getConnection(connURL);
-        }catch (Exception e) {
+        } catch (Exception e) {
             // Print any exeptions that may occur.
             e.printStackTrace();
         }
--- src/GUI/App.java
+++ src/GUI/App.java
@@ -1,6 +1,10 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -8,6 +12,14 @@ public class App extends Application {
     @Override
     public void start(Stage window) {
         window.setTitle("CodeCademy | Home");
+
+        Button openStudentWindow = new Button("Studenten");
+
+        VBox vbox = new VBox(openStudentWindow);
+        Scene scene = new Scene(vbox, 800, 500);
+        vbox.setAlignment(Pos.CENTER);
+
+        window.setScene(scene);
         window.show();
     }
 
