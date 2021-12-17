c9c601299b6e27cfd4f51aece42c66f808b4a37c
Tristan Goossens <d267316@edu.rocwb.nl>
2021-12-17 15:03:15 +0100

    added student list method returning all students

--- src/Database/StudentModel.java
+++ src/Database/StudentModel.java
@@ -7,4 +7,22 @@ public class StudentModel extends Conn {
         // Initialize super class conn
         super();
     }
+
+    public ResultSet getStudents() {
+        // Set query to exectute
+        String query = "SELECT * FROM Student";
+        ResultSet rs = null;
+
+        // Create a prepared statement for the SQL query
+        try (PreparedStatement stmt = super.conn.prepareStatement(query)) {
+            // Execute the prepared query
+            rs = stmt.executeQuery();
+        } catch (Exception e) {
+            // Catch any exeptions
+            System.out.format("Error while getting students (getStudents): %s", e.toString());
+        }
+
+        // Return
+        return rs;
+    }
 }
