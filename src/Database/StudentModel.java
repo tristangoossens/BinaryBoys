c9a872b55593dcef457a10db4ec84450cfab7f2c
Florian Burg <fha.burg@student.avans.nl>
2021-12-20 14:12:44 +0100

    student crud queries added
    crud queries added to Student.java

--- src/Database/StudentModel.java
+++ src/Database/StudentModel.java
@@ -10,11 +10,128 @@ public class StudentModel extends Conn {
         super();
     }
 
+    public void createStudent(Student student){
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO Students VALUES (");
+        sb.append(student.getEmail());
+        sb.append(",");
+        sb.append(student.getName());
+        sb.append(",");
+        sb.append(student.getBirthDate());
+        sb.append(",");
+        sb.append(student.getGender());
+        sb.append(",");
+        sb.append(student.getAdress());
+        sb.append(",");
+        sb.append(student.getCity());
+        sb.append(",");
+        sb.append(student.getCountry());
+        sb.append(")");
+
+        System.out.println(sb.toString());
+
+        String query = sb.toString();
+
+        try(PreparedStatement stmt = super.conn.prepareStatement(query)){
+            stmt.executeQuery();
+            if(!stmt.executeQuery().wasNull()){
+                System.out.println("Student created");
+            }
+        }
+        catch(Exception e){
+            System.out.format("Error while creating student (createStudent): %s", e.toString());
+        }
+
+    }
+
+    public Student readStudent(Student student){
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT * FROM Student WHERE Email = ");
+        sb.append(student.getEmail());
+
+        System.out.println(sb.toString());
+
+        String query = sb.toString();
+
+        try(PreparedStatement stmt = super.conn.prepareStatement(query)){
+            stmt.executeQuery();
+            if(!stmt.executeQuery().wasNull()){
+                System.out.println("Student created");
+            }
+        }
+        catch(Exception e){
+            System.out.format("Error while creating student (createStudent): %s", e.toString());
+        }
+        return 
+    }
+
+    public void updateStudent(Student student){
+        StringBuilder sb = new StringBuilder();
+        sb.append("UPDATE Students SET (");
+        sb.append("Email = ");
+        sb.append(student.getEmail());
+        sb.append(",");
+        sb.append("Name = ");
+        sb.append(student.getName());
+        sb.append(",");
+        sb.append("Birthdate = ");
+        sb.append(student.getBirthDate());
+        sb.append(",");
+        sb.append("Gender = ");
+        sb.append(student.getGender());
+        sb.append(",");
+        sb.append("Address = ");
+        sb.append(student.getAdress());
+        sb.append(",");
+        sb.append("City = ");
+        sb.append(student.getCity());
+        sb.append(",");
+        sb.append("Country = ");
+        sb.append(student.getCountry());
+        sb.append(" WHERE ");
+        sb.append("Email = ");
+        sb.append(student.getEmail());
+        sb.append(")");
+
+        System.out.println(sb.toString());
+
+        String query = sb.toString();
+
+        try(PreparedStatement stmt = super.conn.prepareStatement(query)){
+            stmt.executeQuery();
+            System.out.println("Student updated");
+        }
+        catch(Exception e){
+            System.out.format("Error while updating student (updateStudent): %s", e.toString());
+        }
+    }
+
+    public void deleteStudent(Student student){
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("DELETE FROM Student WHERE Email =");
+        sb.append(student.getEmail());
+
+        System.out.println(sb.toString());
+
+        String query = sb.toString();
+
+        try(PreparedStatement stmt = super.conn.prepareStatement(query)){
+            stmt.executeQuery();
+            if(!stmt.executeQuery().wasNull()){
+                System.out.println("Student deleted");
+            }
+        }
+        catch(Exception e){
+            System.out.format("Error while deleting student (deleteStudent): %s", e.toString());
+        }
+
     public ArrayList<Student> getStudents() {
         // Set query to exectute
         String query = "SELECT * FROM Student";
         ResultSet rs = null;
-        
+
         ArrayList<Student> students = new ArrayList<>();
 
         // Create a prepared statement for the SQL query
--- src/Domain/Student.java
+++ src/Domain/Student.java
@@ -35,6 +35,8 @@ public class Student {
             "}";
     }
 
+    
+
     public String getEmail() {
         return this.email;
     }
