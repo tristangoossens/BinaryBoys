6fe99b024a8eae66c397431c8aa554caf6736571
Tristan Goossens <d267316@edu.rocwb.nl>
2022-01-05 18:57:33 +0100

    added base functionality for webcast pages

---
+++ src/GUI/Webcast/CreateWebcast.java
@@ -0,0 +1,10 @@
+package GUI.Webcast;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CreateWebcast {
+    public Scene getView(Stage stage){
+        return null;
+    }
+}
---
+++ src/GUI/Webcast/EditWebcast.java
@@ -0,0 +1,11 @@
+package GUI.Webcast;
+
+import Domain.Webcast;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EditWebcast {
+    public static Scene getView(Stage stage, Webcast webcast){
+        return null;
+    }
+}
