8bf3c407e456c109ec372818ebb2379d694b1753
Tristan Goossens <d267316@edu.rocwb.nl>
2022-01-05 18:56:34 +0100

    added a insert and update for module

--- src/Database/ContentItemModel.java
+++ src/Database/ContentItemModel.java
@@ -13,6 +13,86 @@ public class ContentItemModel extends Conn {
         super();
     }
 
+    public ArrayList<String> getContactPersonEmails() {
+        // Create prepared statement
+        String query = "SELECT Email FROM Module_Person";
+
+        // Create list for contact person emails
+        ArrayList<String> emails = new ArrayList<>();
+
+        try(PreparedStatement stmt = super.conn.prepareStatement(query)) {
+            // Execute statement
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                emails.add(rs.getString("Email"));
+            }
+
+            // return list of names
+            return emails;
+        } catch(Exception e) {
+            System.out.format("Error while retrieving contact person emails names (getCourseNames): %s", e.toString());
+        }
+
+        // return false on error (nothing is yet returned)
+        return null;
+    }
+
+    public boolean createModule(Module module){
+        String contentItemQuery = "INSERT INTO Content_Item VALUES(NULL, ?, ? ,?, ?)";
+        String moduleQuery = "INSERT INTO Module VALUES(?, ? ,?, ?)";
+
+        // Create a prepared statement to prevent SQL injections
+        try (PreparedStatement stmt = conn.prepareStatement(contentItemQuery, Statement.RETURN_GENERATED_KEYS); PreparedStatement moduleStmt = conn.prepareStatement(moduleQuery)) {
+            // Set auto commit off so the executed queries are executes in a transaction.
+            conn.setAutoCommit(false);
+            
+            // Set data in prepared statement.
+            stmt.setString(1, module.getTitle());
+            stmt.setString(2, module.getStatus());
+            stmt.setDate(3, new java.sql.Date(module.getPublicationDate().getTime()));
+            stmt.setString(4, module.getDescription());
+
+            // Execute first prepared statement
+            stmt.executeUpdate();
+
+            // Get the generated key and use it in the next insert
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next())
+            {
+                int insertedID = rs.getInt(1);
+
+                // Set data for second prepared statement
+                moduleStmt.setInt(1, insertedID);
+                moduleStmt.setString(2, module.getContactPerson());
+                moduleStmt.setInt(3, module.getOrderNumber());
+                moduleStmt.setDouble(4, module.getVersion());
+
+                // Execute the prepared query
+                moduleStmt.executeUpdate();
+
+                // Commit changes on success
+                conn.commit();
+                conn.setAutoCommit(true);
+
+                // Return true on success
+                return true;
+            }
+        } catch(Exception e){
+            try {
+                // Error! Rolling the transaction
+                conn.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+
+            System.out.format("Error while inserting module (createModule): %s", e.toString());
+        }
+
+        // Return false on error (nothing is returned)
+        return false;
+    }
+
     public ArrayList<Module> getModulesForCourse(Course course){
         // Query to retrieve all content items for a course
         String query = "SELECT * FROM Content_Item AS CI INNER JOIN Module AS M ON CI.ID = M.Content_Item_ID INNER JOIN Module_Person AS MP ON M.Module_Person_Email = MP.Email WHERE CI.Course_Name = ?";
@@ -87,6 +167,54 @@ public class ContentItemModel extends Conn {
         // Return null on error (code reached catch block)
         return null;
     }
+
+    public boolean updateModule(Module module){
+        String contentItemQuery = "UPDATE Content_Item SET Title = ?, Status = ?, Publication_Date = ?, Description = ?";
+        String moduleQuery = "UPDATE Module SET Module_Person_Email = ?, Sequence_Number = ?, Version = ?";
+
+        // Create a prepared statement to prevent SQL injections
+        try (PreparedStatement stmt = conn.prepareStatement(contentItemQuery); PreparedStatement moduleStmt = conn.prepareStatement(moduleQuery)) {
+            // Set auto commit off so the executed queries are executes in a transaction.
+            conn.setAutoCommit(false);
+            
+            // Set data in prepared statement.
+            stmt.setString(1, module.getTitle());
+            stmt.setString(2, module.getStatus());
+            stmt.setDate(3, new java.sql.Date(module.getPublicationDate().getTime()));
+            stmt.setString(4, module.getDescription());
+
+            // Execute first prepared statement
+            stmt.executeUpdate();
+
+            // Set data for second prepared statement
+            moduleStmt.setString(1, module.getContactPerson());
+            moduleStmt.setInt(2, module.getOrderNumber());
+            moduleStmt.setDouble(3, module.getVersion());
+
+            // Execute the prepared query
+            moduleStmt.executeUpdate();
+
+            // Commit changes on success
+            conn.commit();
+            conn.setAutoCommit(true);
+
+            // Return true on success
+            return true;
+        } catch(Exception e){
+            try {
+                // Error! Rolling back the transaction
+                conn.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+
+            System.out.format("Error while updating module (updateModule): %s", e.toString());
+        }
+
+        // Return false on error (nothing is returned)
+        return false;
+    }
+
 
     public boolean deleteContentItem(int ID){
         String query = "DELETE FROM Content_Item WHERE ID = ?";
