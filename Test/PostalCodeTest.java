48e41618190e623535da4e2f74e9a49a5e6224a6
Tristan Goossens <d267316@edu.rocwb.nl>
2022-01-15 19:11:17 +0100

    added remaining test cases for postal code

--- Test/PostalCodeTest.java
+++ Test/PostalCodeTest.java
@@ -5,42 +5,57 @@ import org.junit.Test;
 import Validation.PostalCode;
 
 public class PostalCodeTest {
-    // Test if passing null returns a nullpointerException
     @Test(expected = NullPointerException.class)
     public void testformatPostalCodeRequiresNullSignalsNullPointerException() {
+        // Execute
         PostalCode.formatPostalCode(null);
     }
 
-    // Test if a valid postal code returns a formatted postal code
     @Test
     public void testformatPostalCodeRequires1234aAEnsures1234_AA(){
-        String postalCode = "1234aA";
+        // Execute
+        String result = PostalCode.formatPostalCode("1234aA");
 
-        String result = PostalCode.formatPostalCode(postalCode);
-
+        // Assert
         assertEquals("1234 AA", result);
     }
 
-    // Test if a valid postal code returns a formatted postal code
     @Test
     public void testformatPostalCodeRequires4321_ZBEnsures4321_ZB(){
-        String postalCode = "4321 ZB";
+        // Execute
+        String result = PostalCode.formatPostalCode("4321 ZB");
 
-        String result = PostalCode.formatPostalCode(postalCode);
-
+        // Assert
         assertEquals("4321 ZB", result);
     }
 
-    // Test if a passing a invalid postal code will return a illegalargumentexeption
     @Test(expected = IllegalArgumentException.class)
     public void testformatPostalCodeRequires1234A$SignalsIllegalArgumentException() {
+        // Execute
         PostalCode.formatPostalCode("1234A$");
     }
 
-    // TODO: Add testcases for
-    // Checking out of bounds numbers
-    // Checking postal codes with fewer numbers (333 AB)
-    // Checking postal codes with fewer letters (3333 A)
-    // Checking postal codes with more numbers (33333 AB)
-    // Checking postal codes with more letters (3333 ABA)
+    @Test(expected = IllegalArgumentException.class)
+    public void testformatPostalCodeRequires10000ABSignalsIllegalArgumentException() {
+        // Execute
+        PostalCode.formatPostalCode("10000AB");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testformatPostalCodeRequires333ABSignalsIllegalArgumentException() {
+        // Execute
+        PostalCode.formatPostalCode("333AB");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testformatPostalCodeRequires1000ASignalsIllegalArgumentException() {
+        // Execute
+        PostalCode.formatPostalCode("1000A");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testformatPostalCodeRequires1000ABASignalsIllegalArgumentException() {
+        // Execute
+        PostalCode.formatPostalCode("1000ABA");
+    }
 }
