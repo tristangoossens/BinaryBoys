0bdefba47f86ce435340132b18ccb4df406e1846
Tristan Goossens <d267316@edu.rocwb.nl>
2022-01-16 20:09:18 +0100

    add grade validation

---
+++ src/Validation/GradeValidation.java
@@ -0,0 +1,28 @@
+package Validation;
+
+public class GradeValidation {
+    /**
+     * @desc Checks if the given grade is properly formatted. This function will return true on succes
+     *       And will return false when entering an incorrect grade.
+     * 
+     * @subcontract valid url
+     *   @requires grade >= 1 &&
+     *             grade <= 10;
+     *   @ensures \result = true
+     * 
+     *  
+     * @subcontract invalid grade
+     *   @requires no other valid precondition;
+     *   @ensures \ result = false;
+     * 
+     */
+    public static boolean validateGrade(int grade) {
+        // Check if the grade is lower than 1 and higher than 10
+        if (grade < 1 && grade > 10) {
+            return false;
+        }
+
+        // If all conditions are met, return true
+        return false;
+    }
+}
